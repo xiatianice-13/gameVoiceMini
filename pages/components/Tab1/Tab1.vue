3b582d0b365aa32a05bc0b6dc7383a67e49675db
wjh <junhao.wang@cqgbjk.com>
2024-08-27 18:23:37 +0800

    frame

--- pages/components/Tab1/Tab1.vue
+++ pages/components/Tab1/Tab1.vue
@@ -1,26 +1,120 @@
 <template>
-  <div>
-    <h1>Tab {{typeid+1}} Content</h1>
-    <p>This is the content for Tab 1.</p>
-  </div>
+	<view>
+		<!-- <view v-for="(item, index) in tabBars" :key="item.id"> -->
+			<uni-grid :column="3" :highlight="true">
+				<uni-grid-item v-for="(item, index) in tabBars" :index="index" :key="index" 
+				marker="image" :hor="35" :ver="-45" :img-width="25" :src="item.icon">
+					<view class="grid-item-box" style="background-color: #fff;">
+						<uni-icons type="image" :size="30" color="#777" :custom-prefix="item.icon" />
+						<text class="text">{{item.name}}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		<!-- </view> -->
+	</view>
 </template>
 
 <script>
-export default {
-  props: {
-    typeid: {
-      type: Number,
-      default: 1
-    }
-  },
-  data() {
-    return {
-      name: 'Tab1'
-    };
-  }
-};
+	export default {
+		props: {
+			typeid: {
+				type: Number,
+				default: 1
+			}
+		},
+		data() {
+			return {
+				name: 'Tab1',
+				tabBars: [],
+				avatar: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png',
+				extraIcon: {
+					color: '#4cd964',
+					size: '22',
+					type: 'gear-filled'
+				}
+			};
+		},
+		created() {
+			console.log(this.typeid);
+			this.tabBars = [{
+					id: 3,
+					name: 'CSGO',
+					icon: 'https://gullu-edu.oss-cn-hangzhou.aliyuncs.com/gameicon/CS2.jpg'
+				},
+				{
+					id: 4,
+					name: 'LOL',
+					icon: 'https://gullu-edu.oss-cn-hangzhou.aliyuncs.com/gameicon/CS2.jpg'
+				},
+				{
+					id: 5,
+					name: '瓦罗兰特',
+					icon: 'https://gullu-edu.oss-cn-hangzhou.aliyuncs.com/gameicon/CS2.jpg'
+				},
+			]
+		},
+		methods: {
+			onClick(e) {
+				console.log(e)
+			},
+
+		},
+	};
 </script>
 
-<style scoped>
-/* Tab1 specific styles */
+<style lang="scss">
+	.container {
+		overflow: hidden;
+	}
+
+	.custom-cover {
+		flex: 1;
+		flex-direction: row;
+		position: relative;
+	}
+
+	.cover-content {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 40px;
+		background-color: rgba($color: #000000, $alpha: 0.4);
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding-left: 15px;
+		font-size: 14px;
+		color: #fff;
+	}
+
+	.card-actions {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: center;
+		height: 45px;
+		border-top: 1px #eee solid;
+	}
+
+	.card-actions-item {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.card-actions-item-text {
+		font-size: 12px;
+		color: #666;
+		margin-left: 5px;
+	}
+
+	.cover-image {
+		flex: 1;
+		height: 150px;
+	}
+
+	.no-border {
+		border-width: 0;
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,95 +1,101 @@
 <template>
-  <div id="app">
-    <!-- Tabs Row -->
-    <div class="tabs-container">
-      <div
-        v-for="(item, index) in tabBars"
-        :key="item.id"
-        class="tab-item"
-        :class="tabIndex === index ? 'active' : ''"
-        @click="changeTab(index)"
-      >
-        {{ item.name }}
-      </div>
-    </div>
+	<div id="app">
+		<!-- Tabs Row -->
+		<div class="tabs-container">
+			<div v-for="(item, index) in tabBars" :key="item.id" class="tab-item"
+				:class="tabIndex === index ? 'active' : ''" @click="changeTab(index)">
+				{{ item.name }}
+			</div>
+		</div>
 
-    <!-- Content Area -->
-    <div class="content-area">
-      <component :is="currentComponent" :typeid="tabIndex"></component>
-    </div>
-  </div>
+		<!-- Content Area -->
+		<div class="content-area">
+			<component :is="currentComponent" :typeid="tabIndex"></component>
+		</div>
+	</div>
 </template>
 
 <script>
-import Tab1 from '../components/Tab1/Tab1.vue';
-import Tab2 from '../components/Tab2/Tab2.vue';
-import Tab3 from '../components/Tab3/Tab3.vue';
+	import Tab1 from '../components/Tab1/Tab1.vue';
+	import Tab2 from '../components/Tab2/Tab2.vue';
 
-export default {
-  data() {
-    return {
-      tabBars: [],
-      tabIndex: 0,
-    };
-  },
-  computed: {
-    currentComponent() {
-      return this.tabBars[this.tabIndex].component;
-    }
-  },
-  onLoad() {
-  	this.tabBars= [
-        { id: 1, name: 'Tab 1', component: Tab1 },
-        { id: 2, name: 'Tab 2', component: Tab2 },
-      ]
-  },
-  methods: {
-    changeTab(index) {
-      this.tabIndex = index;
-    },
-  },
-};
+	export default {
+		data() {
+			return {
+				tabBars: [],
+				tabIndex: 0,
+				componentsMap: {
+					Tab1,
+					Tab2,
+				},
+			};
+		},
+		computed: {
+			currentComponent() {
+				const componentKey = `Tab${this.tabBars[this.tabIndex].id}`;
+				return this.componentsMap[componentKey] || null;
+			}
+		},
+		onLoad() {
+			this.tabBars = [{
+					id: 1,
+					name: '游戏分类'
+				},
+				{
+					id: 2,
+					name: '主播分类'
+				},
+			]
+		},
+		methods: {
+			changeTab(index) {
+				this.tabIndex = index;
+			},
+		},
+	};
 </script>
 
 <style scoped>
-#app {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
+	#app {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
 
-.tabs-container {
-  display: flex;
-  border-bottom: 1px solid #ddd; /* Border for the bottom of tabs */
-}
+	.tabs-container {
+		display: flex;
+		border-bottom: 1px solid #ddd;
+		/* Border for the bottom of tabs */
+	}
 
-.tab-item {
-  flex: 1; /* Distribute width equally */
-  text-align: center;
-  padding: 10px;
-  cursor: pointer;
-  position: relative;
-}
+	.tab-item {
+		flex: 1;
+		/* Distribute width equally */
+		text-align: center;
+		padding: 10px;
+		cursor: pointer;
+		position: relative;
+	}
 
-.tab-item:not(:last-child)::after {
-  content: '';
-  position: absolute;
-  right: 0;
-  top: 50%;
-  width: 1px;
-  height: 50%;
-  background-color: #ddd;
-  transform: translateY(-50%);
-}
+	.tab-item:not(:last-child)::after {
+		content: '';
+		position: absolute;
+		right: 0;
+		top: 50%;
+		width: 1px;
+		height: 50%;
+		background-color: #ddd;
+		transform: translateY(-50%);
+	}
 
-.active {
-  color: #007aff;
-  font-weight: bold;
-}
+	.active {
+		color: #007aff;
+		font-weight: bold;
+	}
 
-.content-area {
-  flex: 1;
-  padding: 20px;
-  background-color: #ffffff;
-}
+	.content-area {
+		flex: 1;
+		padding: 20px;
+		background-color: #ffffff;
+	}
 </style>
